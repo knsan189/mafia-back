3d18f0a52ddceecd4173d93223b9dc6950dc2e5f
hnjin <knsan189@gmail.com>
2023-04-12 15:56:19 +0900

    개발 환경 수정, view 파일 삭제

--- app.ts
+++ app.ts
@@ -1,30 +1,14 @@
-/* eslint-disable no-underscore-dangle */
-import { fileURLToPath } from "url";
 import cors from "cors";
-import log4js from "log4js";
 import createError from "http-errors";
 import express from "express";
-import path from "path";
 import cookieParser from "cookie-parser";
-import logger from "morgan";
-import IndexRouter from "./src/routes/index.js";
-
-const isDev = `${process.env.NODE_ENV}`.trim() === "development";
 
 const app = express();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-// log4js.configure(path.join(__dirname, isDev ? "log4js.json" : "../log4js.json"));
-app.set("views", path.join(__dirname, "src/views"));
-app.set("view engine", "pug");
-app.use(logger(isDev ? "dev" : "common"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, isDev ? "public" : "../public")));
-// app.use("*", IndexRouter);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
