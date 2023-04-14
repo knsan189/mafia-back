d38818908c0e2de005ebe31bc3e8668050a48481
Jin Haneul <knsan189@naver.com>
2023-04-14 20:45:20 +0900

    로그 수정

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -1,4 +1,4 @@
 export default function MaifaLog(...text: string[]) {
   // eslint-disable-next-line no-console
-  console.log(`[${new Date().toLocaleString()}]`, text);
+  console.log(`[${new Date().toLocaleString()}]`, ...text);
 }
