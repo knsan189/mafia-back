c74c5fc126190b24c1ee9525b67f7b7f7c9cee06
hnjin <knsan189@gmail.com>
2023-06-07 11:04:07 +0900

    정적 파일 라우터 변경

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "../public")));
+app.use("/static", express.static(path.join(__dirname, "../public")));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
